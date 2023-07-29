1bcbbe6c6e701c6ecc1eaea140fb3660dd1dbafd
Filipe Pontes <106703491+filipetpontes@users.noreply.github.com>
2023-07-29 00:40:09 -0300

    Add files via upload

--- pages/4_📞_Contato.py
+++ pages/4_📞_Contato.py
@@ -21,8 +21,8 @@ st.header("📞 Contato")
 
 st.markdown("#### Arthur Lira")
 st.markdown("[![Linkedin](https://img.shields.io/badge/LinkedIn-blue)](https://www.linkedin.com/in/arthur-santos-lira-084292145/)")
-st.markdown("[![GitHub](https://img.shields.io/badge/GitHub-black)](https://github.com/arthurrslira/)")
+st.markdown("[![GitHub](https://img.shields.io/badge/GitHub-gray)](https://github.com/arthurrslira/)")
 
 st.markdown("#### Filipe Pontes")
 st.markdown("[![LinkedIn](https://img.shields.io/badge/LinkedIn-blue)](https://www.linkedin.com/in/filipetpontes/)")
-st.markdown("[![GitHub](https://img.shields.io/badge/GitHub-black)](https://github.com/filipetpontes)")
+st.markdown("[![GitHub](https://img.shields.io/badge/GitHub-gray)](https://github.com/filipetpontes)")
